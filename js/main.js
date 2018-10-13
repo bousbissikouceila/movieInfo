b21151c3d8c0ca5a2eed57d1fc2756ee8ab887d1
Kouceïla BOUSIBISSI <bousbissi.kouceila@gmail.com>
2018-10-13 13:04:51 +0100

    Added movie.html and modified style.css & index.html ÃÃ& main.js

--- js/main.js
+++ js/main.js
@@ -23,8 +23,8 @@ function getMovies(searchText) {
                 if (index % 4 == 0) output += `<div class="row">`;
                 output += `
             <div class="col-md-3">
-              <div class="card card-body bg-light text-center">                
-                  <img src="${movie.Poster}">
+              <div class="card card-body bg-light text-center movie-card">                
+                  <img src="${movie.Poster}" class="card-img" alt="${movie.Title} poster">
                   <h5>${movie.Title}</h5>
                   <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>                
               </div>
@@ -37,6 +37,55 @@ function getMovies(searchText) {
             });
             console.log(counter);
             $('#movies').html(output);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+function movieSelected(id) {
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movie.html';
+    return false;
+}
+
+function getMovie() {
+    const movieId = sessionStorage.getItem('movieId');
+    axios.get(`http://www.omdbapi.com/?apikey=905820ff&i=${movieId}`)
+        .then((response) => {
+            console.log(response);
+            const movie = response.data;
+            const output = `
+            <div class= "row">
+               <div class="col-md-4">
+                 <img src="${movie.Poster}" class="card card-img bg-dark" alt="${movie.Title} poster">
+               </div>
+               <div class ="col-md-8">
+                    <div class="card-title text-center">
+                        <h2>${movie.Title}</h2>
+                    </div>
+                    <ul class="list-group">
+                        <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+                        <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+                        <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+                        <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+                        <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+                        <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+                        <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+                    </ul>
+               </div>
+            </div>
+            <div class="row">
+                <div class="card card-body bg-transparent">
+                    <h3>Plot</h3>
+                    <p> ${movie.Plot}</p>
+                    <hr>
+                    <a href="http://imdb.com/title/${movie.imdbID}" target="blank" class="btn btn-primary">View IMDB</a>
+                    <a href="index.html" class="btn btn-default">Go Back To Search</a>
+                </div>
+            </div>
+            `;
+            $('#movie').html(output);
         })
         .catch((err) => {
             console.log(err);
